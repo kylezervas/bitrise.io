4efc90dda52d01218e8612cda54100f892b0151e
Kyle Zervas <75883799+kylezervas@users.noreply.github.com>
2021-02-28 08:42:17 -0600

    Auto-commit: Sun Feb 28 2021 08:42:11 GMT-0600 (Central Standard Time)

---
+++ superQuery/kylezervas7@gmail.com/My first dashboard/Multi Query.sql
@@ -0,0 +1,12 @@
+/*Run multiple queries in parallel or in a sequence.
+Just separate each query with a semicolon, like in the example below
+SELECT
+  1;
+SELECT
+  2;
+SELECT
+  3;
+SELECT
+  4;
+SELECT
+  5;.*/
